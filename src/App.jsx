54be259530ad858471f503f2029c194c8864e307
Vicky-peace <victorpeace47@gmail.com>
2024-02-02 08:27:13 +0300

    new mod

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [board, setBoard] = useState(["","","","" ,"","","","",""])
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <div className="board">
+          <div className="row"></div>
+          <div className="row"></div>
+          <div className="row"></div>
+      </div>
+    </div>
   )
 }
 
